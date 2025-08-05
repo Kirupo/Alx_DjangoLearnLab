8fe98eda4037359d32c1c1927b4af82657e1ac57
Kirupo <kirubelg217@gmail.com>
2025-08-05 15:58:43 +0300

    Add files via upload

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -1,5 +1,5 @@
+from django_filters import rest_framework as django_filters
 from rest_framework import generics, permissions, filters
-from django_filters.rest_framework import DjangoFilterBackend
 from .models import Author, Book
 from .serializers import AuthorSerializer, BookSerializer
 
@@ -18,25 +18,14 @@ class AuthorDetailView(generics.RetrieveAPIView):
 # ----------------- Book Views -----------------
 
 class BookListView(generics.ListAPIView):
-    """
-    Book List View:
-    - Supports filtering by title, author name, publication year.
-    - Allows search by title or author name.
-    - Allows ordering by title or publication year.
-
-    Example queries:
-    /api/books/?title=Harry Potter
-    /api/books/?search=Rowling
-    /api/books/?ordering=-publication_year
-    """
     queryset = Book.objects.all()
     serializer_class = BookSerializer
     permission_classes = [permissions.IsAuthenticatedOrReadOnly]
-    filter_backends = [DjangoFilterBackend, filters.SearchFilter, filters.OrderingFilter]
+    filter_backends = [django_filters.DjangoFilterBackend, filters.SearchFilter, filters.OrderingFilter]
     filterset_fields = ['title', 'author__name', 'publication_year']
     search_fields = ['title', 'author__name']
     ordering_fields = ['title', 'publication_year']
-    ordering = ['title']  # Default ordering
+    ordering = ['title']
 
 class BookDetailView(generics.RetrieveAPIView):
     queryset = Book.objects.all()
